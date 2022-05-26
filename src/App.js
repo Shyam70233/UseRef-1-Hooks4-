96c13c217b52f52a5e9efe7cbc442f19d4edf979
Shyam70233 <deluxshop468@gmail.com>
2022-05-26 19:10:58 +0530

    hook6- reducer

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
-import React, { useRef } from 'react';
+import React, { useReducer } from 'react';
 import './style.css';
-
+const initialState = 0;
+function reducer(state = initialState, action) {
+  if (action.type === '+') {
+    return state + 1;
+  }
+  if (action.type === '-') {
+    return state - 1;
+  }
+  if (action.type === '0') {
+    return 0;
+  }
+  if (action.type === '100') {
+    return 100;
+  }
+}
 export default function App() {
-  const ref = useRef();
-  const handleClick = () => {
-    ref.current.innerHTML = 'Welcome Ref in React';
+  const [x, dispatch] = useReducer(reducer, initialState);
+  const p1 = () => {
+    dispatch({ type: '+' });
   };
+  const p2 = () => {
+    dispatch({ type: '-' });
+  };
+  const p3 = () => {
+    dispatch({ type: '0' });
+  };
+  const p4 = () => {
+    dispatch({ type: '100' });
+  };
+
   return (
-    <>
-      <button onClick={handleClick}> Click Me</button>
-      <div ref={ref}>
-        <h1>Hello StackBlitz!</h1>
-        <p>Start editing to see some magic happen :)</p>
-      </div>
-    </>
+    <div>
+      <h1>X : {x}</h1>
+      <button onClick={p1}>+</button>
+      <button onClick={p2}>-</button>
+      <button onClick={p3}>0</button>
+      <button onClick={p4}>100</button>
+    </div>
   );
 }
